e88d88b373329b060b26a74767c60b01f2679e27
Natesz <natesz777@yahoo.com>
2020-04-12 19:06:43 +0200

    deploying burgerapp

---
+++ public/BurgerApp/build/precache-manifest.b26e4bdd45183092e206339270cc260e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262b61f64febedc9563c4b298be5ef7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66c81bb38283c779bc41",
+    "url": "/static/css/main.a753c83e.chunk.css"
+  },
+  {
+    "revision": "ca6cff588b95554a4d31",
+    "url": "/static/js/2.d13c46f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d13c46f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c81bb38283c779bc41",
+    "url": "/static/js/main.63a2cc5a.chunk.js"
+  },
+  {
+    "revision": "328d60cc0e73534627fa",
+    "url": "/static/js/runtime-main.278a9be5.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/original.b8503d26.png"
+  }
+]);
